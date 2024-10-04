50dffe9d7b9644514f3a3dce46bdc98d57b8d68b
sophiaecl <67115618+sophiaecl@users.noreply.github.com>
2024-10-04 17:41:54 +0200

    chang any in logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 
-const Logo = (props: any) => {
+const Logo = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg height={32} viewBox="0 0 120 28" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
